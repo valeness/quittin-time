793c7ef86e1b4ea8cb048c2d14534133d10693f3
James <valeness84@gmail.com>
2015-02-07 23:26:00 -0600

    Added a Previous Times Tracker

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ gtk.gdk.threads_init()
 
 hellon = 0
 counter = 0
+old_s = []
 running = True
 
 class HelloWorld(gtk.Window):
@@ -23,6 +24,8 @@ class HelloWorld(gtk.Window):
 		self.set_position(gtk.WIN_POS_CENTER)
 		#self.set_border_width(20)
 		
+		prev_text = "Previous Times: \n"
+
 		vbox = gtk.VBox(False, 2)
 
 		mb = gtk.MenuBar()
@@ -36,17 +39,19 @@ class HelloWorld(gtk.Window):
 
 		vbox.pack_start(mb, False, False, 0)
 
-		table = gtk.Table(5, 2, True)
+		table = gtk.Table(5, 6, True)
 
 		button = gtk.ToggleButton(label = "Start")
 		button2 = Button("Test")
 
 		text = Label("Hello World")
 		text.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#FFF'))
+		previous = Label(prev_text)
+		previous.modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse('#FFF'))
 
-		table.attach(button, 0, 4, 1, 2)
-		#table.attach(button2, 0, 4, 2, 3)
-		table.attach(text, 0, 4, 0, 1)
+		table.attach(button, 0, 3, 1, 2)
+		table.attach(text, 0, 3, 0, 1)
+		table.attach(previous, 0, 1, 2, 6)
 
 		vbox.pack_end(table, True, True, 0)
 
@@ -56,6 +61,8 @@ class HelloWorld(gtk.Window):
 
 		self.text = text
 		self.button = button
+		self.previous = previous
+		self.prev_text = prev_text
 
 		self.add(vbox)
 		self.show_all()
@@ -84,6 +91,9 @@ class HelloWorld(gtk.Window):
 			#print i
 			i = i + 1
 			if self.button.get_active() == 0:
+				global display
+				self.prev_text += ("%s \n") % display
+				self.previous.set_text(self.prev_text)
 				running = False
 				break
 
@@ -92,6 +102,7 @@ class HelloWorld(gtk.Window):
 		hour = show / 3600
 		minutes = (show % 3600) / 60
 		seconds = show - (int(hour) * 3600) - (int(minutes) * 60)
+		global display
 		display = "%s : %s : %0.2f" % (str(int(hour)), str(int(minutes)), seconds)
 		self.text.set_text(display)
 
